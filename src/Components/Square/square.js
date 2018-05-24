ecd223e134e9ee083354bc5a47a695e34b639f31
Jizu Sun <jizu.sun@sap.com>
2018-05-24 22:39:57 +0800

    move components to its own folder

---
+++ src/Components/Square/square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Square = (props) => (
+    <button className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  )
+
+
+export default Square
